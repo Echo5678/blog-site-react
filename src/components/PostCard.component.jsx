1f50c851cc6b3020ab22019cf24f29f6646f61eb
Vincent Garcia <vincentgarciajr2712@gmail.com>
2023-02-13 01:51:09 -0800

    Simplified Postcard and Postcardpreview components

--- src/components/PostCard.component.jsx
+++ src/components/PostCard.component.jsx
@@ -1,16 +1,16 @@
 import ReactMarkdown from 'react-markdown';
 
-export default function PostCard({post}) {
+export default function PostCard({post:{title, date, body}}) {
     return (
     <div className="div-size mt-3 mx-auto">
         <div className="flex justify-between mb-4 items-center mt-1">
-        <h3>{post.title}</h3>
+        <h3>{title}</h3>
         <ReactMarkdown>
-            {post.date}
+            {date}
         </ReactMarkdown>
         </div>
         <ReactMarkdown>
-            {post.body}
+            {body}
         </ReactMarkdown>
     </div>);
   }
